16817269fd331b9d558404b1985bb33d353013ac
rahulaes17 <rahulaes1704@gmail.com>
2026-06-22 23:59:38 +0530

    Pattern 4

---
+++ Patterns/pattern4.java
@@ -0,0 +1,15 @@
+public class pattern4 {
+    public void result(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern4 res = new pattern4();
+        res.result(5);
+    }
+}
