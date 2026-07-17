962acd4f102987093fe9d70e00c4cd1ba19097c3
rahulaes17 <rahulaes1704@gmail.com>
2026-07-17 23:59:13 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class BubbleSort{
+    public static void sort(int[] arr){
+        for (int i = 0; i < arr.length-2; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,2,4,78,23,65,34,53};
+        sort(arr);
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
