770ef6d57e15f95cc005489ee968fed265fc8dca
rahulaes17 <rahulaes1704@gmail.com>
2026-08-06 23:54:29 +0530

    Remove Duplicates in-place from Sorted Array

---
+++ Arrays/checkDuplicate.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class checkDuplicate {
+    public int duplicateCheck(int[] arr){
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[index] != arr[i]){
+                index++;
+                arr[index] = arr[i];
+            }
+        }
+        return index + 1 ;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,2,3,3,4,4,5,5,5,5,6};
+        checkDuplicate res = new checkDuplicate();
+        int k = res.duplicateCheck(arr);
+        System.out.println("No. of unique elements = " + k);
+        
+    }
+}
