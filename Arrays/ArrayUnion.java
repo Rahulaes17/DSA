a271ef3efc5433792b0f8744b5ccd7e16e6b3a50
rahulaes17 <rahulaes1704@gmail.com>
2026-08-14 23:30:52 +0530

    Approach 2

--- Arrays/ArrayUnion.java
+++ Arrays/ArrayUnion.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class ArrayUnion {
+/*class ArrayUnion {
     public List<Integer> FindUnion(int[] arr1, int[] arr2, int n, int m) {
         TreeMap<Integer, Integer> freq = new TreeMap<>();
         for (int i = 0; i < n; i++)
@@ -23,4 +23,35 @@ class ArrayUnion {
         for (int val : Union)
             System.out.print(val + " ");
     }
+} */
+
+    import java.util.*;
+
+class Solution {
+    public List<Integer> findUnion(int[] arr1, int[] arr2) {
+        Set<Integer> st = new TreeSet<>();
+        for (int num : arr1) {
+            st.add(num);
+        }
+        for (int num : arr2) {
+            st.add(num);
+        }
+        return new ArrayList<>(st);
+    }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr2 = {2, 3, 4, 4, 5, 11, 12};
+
+        Solution obj = new Solution();
+        List<Integer> result = obj.findUnion(arr1, arr2);
+
+        System.out.print("Union of arr1 and arr2 is: ");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+    }
+}
+
