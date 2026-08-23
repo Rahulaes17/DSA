28ff719df666caba4993acdad49d718b4be2a4ea
rahulaes17 <rahulaes1704@gmail.com>
2026-08-23 23:22:59 +0530

    Count Maximum Consecutive One's in the array

---
+++ Arrays/consectiveOnes.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int cnt = 0;
+        int maxi = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            maxi = Math.max(maxi, cnt);
+        }
+        return maxi;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        Solution obj = new Solution();
+        int ans = obj.findMaxConsecutiveOnes(nums);
+        System.out.println("The maximum consecutive 1's are " + ans);
+    }
+}
