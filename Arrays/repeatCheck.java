0f9cfb20b08b9792237d6327f4d9765e4ac51dbc
rahulaes17 <rahulaes1704@gmail.com>
2026-08-25 23:23:43 +0530

    Find the number that appears once, and the other numbers twice

---
+++ Arrays/repeatCheck.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int getSingleElement(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == num)
+                    count++;
+            }
+
+            if (count == 1) return num;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1, 2};
+
+        Solution obj = new Solution();
+        int ans = obj.getSingleElement(arr);
+
+        System.out.println("The single element is: " + ans);
+    }
+}
